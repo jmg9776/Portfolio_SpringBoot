0b0b05565fa65dc8cb3b4e62f0f9bef264344474
jmg97 <jmg97@kakao.com>
2023-12-26 21:17:43 +0900

    feat : BoardService에 getBoardList 반환 메서드 추가

--- src/main/java/com/jmg/blog/application/board/service/BoardService.java
+++ src/main/java/com/jmg/blog/application/board/service/BoardService.java
@@ -1,14 +1,18 @@
 package com.jmg.blog.application.board.service;
 
+import com.jmg.blog.application.board.response.BoardCategoryInfoResponse;
 import com.jmg.blog.domain.board.repository.BoardInfoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class BoardService {
     private final BoardInfoRepository boardInfoRepository;
-    public void getBoardList(String category) {
-        boardInfoRepository.findBoardInfoListByBoardCategory(category);
+
+    public List<BoardCategoryInfoResponse> getBoardList(String category) {
+        return boardInfoRepository.findBoardInfoListByBoardCategory(category);
     }
 }

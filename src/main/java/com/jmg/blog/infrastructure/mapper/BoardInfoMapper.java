f3eba083ed105c7e2212fda1eb1e8d228eac16d2
jmg97 <jmg97@kakao.com>
2023-12-26 10:19:49 +0900

    add 보드인포 매퍼 추가

---
+++ src/main/java/com/jmg/blog/infrastructure/mapper/BoardInfoMapper.java
@@ -0,0 +1,20 @@
+package com.jmg.blog.infrastructure.mapper;
+
+import com.jmg.blog.application.board.response.BoardInfoResponse;
+import com.querydsl.core.Tuple;
+import org.mapstruct.Mapper;
+
+import java.util.Optional;
+
+import static com.jmg.blog.domain.board.model.QBoard.board;
+import static com.jmg.blog.domain.board.model.QBoardPost.boardPost;
+import static com.querydsl.core.types.ExpressionUtils.count;
+
+@Mapper(componentModel = "spring")
+public interface BoardInfoMapper {
+    default BoardInfoResponse toBoardInfoResponse(Tuple tuple) {
+        String boardName = Optional.ofNullable(tuple.get(board.name)).orElse("No Name");
+        Long postCount = Optional.ofNullable(tuple.get(count(boardPost.id))).orElse(0L);
+        return new BoardInfoResponse(boardName, postCount);
+    }
+}

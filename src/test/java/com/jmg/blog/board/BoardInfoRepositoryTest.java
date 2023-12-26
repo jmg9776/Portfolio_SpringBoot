e7f38e4f1ab703a85651221db7ed6de2f7d7cde6
jmg97 <jmg97@kakao.com>
2023-12-26 10:23:45 +0900

    test: 보드 정보 조회 기능과 카테고리별 조회 기능 테스트 추가
    - '보드의 정보 가져오기' 테스트를 추가하여 'findBoardInfo' 메소드의 정확성을 검증합니다. 이 테스트는 존재하지 않는 컨텐츠에 대한 조회, 특정 텍스트로 조회, 그리고 전체 조회의 세 가지 시나리오를 확인합니다.
    - '카테고리 기준으로 보드 정보 가져오기' 테스트를 추가하여 'findBoardInfoListByBoardCategory' 메소드의 기능을 검증합니다. 이 테스트는 null 입력을 통해 모든 카테고리의 보드 정보를 가져오는 기능을 확인합니다.
    - 각 테스트 케이스의 목적과 기대하는 결과에 대한 상세한 설명을 @DisplayName 어노테이션으로 제공합니다.
    - 로깅을 통해 테스트 동작 중에 발생하는 정보를 추적합니다.

---
+++ src/test/java/com/jmg/blog/board/BoardInfoRepositoryTest.java
@@ -0,0 +1,50 @@
+package com.jmg.blog.board;
+
+import com.jmg.blog.application.board.response.BoardInfoResponse;
+import com.jmg.blog.domain.board.repository.BoardInfoRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+public class BoardInfoRepositoryTest {
+
+    @Qualifier("boardInfoRepositoryImpl")
+    @Autowired
+    private BoardInfoRepository boardInfoRepository;
+    private static final Logger logger = LoggerFactory.getLogger(BoardInfoRepositoryTest.class);
+
+    @Test
+    @DisplayName("보드의 정보 가져오기")
+    void boardListTest() {
+        String greenText = "프로젝트";
+        List<BoardInfoResponse> red = boardInfoRepository.findBoardInfo("NO CONTENT");
+        List<BoardInfoResponse> green = boardInfoRepository.findBoardInfo(greenText);
+        List<BoardInfoResponse> all = boardInfoRepository.findBoardInfo(greenText);
+
+        logger.info(red.toString());
+        logger.info(green.toString());
+        logger.info(all.toString());
+
+        // 없는 컨텐츠 - red 리스트가 비어있음을 확인
+        assertTrue(red.isEmpty(), "리스트가 비어있지 않습니다. 'NO CONTENT'로 검색했을 때 결과가 없어야 합니다.");
+
+        // green 리스트에 특정 요소가 포함되어 있는지 확인
+        assertTrue(green.stream().anyMatch(board -> greenText.equals(board.boardName())),
+                "리스트에 '" + greenText + "'가 포함되어 있지 않습니다. '프로젝트'로 검색했을 때 해당 이름을 가진 보드가 최소 하나 이상 있어야 합니다.");
+    }
+
+    @Test
+    @DisplayName("카테고리 기준으로 보드 정보 가져오기")
+    void boardListByCategory() {
+        logger.info(boardInfoRepository.findBoardInfoListByBoardCategory(null).toString());
+    }
+}

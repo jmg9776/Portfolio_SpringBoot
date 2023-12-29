9c7afc68137f2ed64e6c64b4c8446b79fe475dfb
jmg97 <jmg97@kakao.com>
2023-12-29 19:18:32 +0900

    docs: BoardInfoService의 주석 명확성 개선
    - findByCategory와 findByBoardName 메서드에 캐싱 메커니즘, 예상되는 파라미터 및 반환 값에 대해 설명하는 상세한 주석 추가.
    - 가독성 향상 및 향후 유지보수자 및 개발자를 위한 컨텍스트 제공 강화.

--- src/main/java/com/jmg/blog/application/board/service/BoardInfoService.java
+++ src/main/java/com/jmg/blog/application/board/service/BoardInfoService.java
@@ -14,11 +14,27 @@ import java.util.List;
 public class BoardInfoService {
     private final BoardInfoRepository boardInfoRepository;
 
+    /**
+     * 주어진 카테고리에 따라 보드 정보를 조회하고 캐시합니다.
+     * 캐시된 데이터는 'boardCategoryInfo' 캐시에 저장되며, 카테고리 이름이 키로 사용됩니다.
+     *
+     * @param category 조회할 보드의 카테고리 이름. null이면 모든 카테고리의 보드를 조회합니다.
+     * @return 주어진 카테고리에 해당하는 보드 정보의 리스트를 반환합니다.
+     *         각 정보에는 카테고리별로 분류된 보드의 이름과 게시글 수가 포함됩니다.
+     */
     @Cacheable(value = "boardCategoryInfo", key = "#category")
     public List<BoardCategoryInfoResponse> findByCategory(String category) {
         return boardInfoRepository.findBoardInfoListByBoardCategory(category);
     }
 
+    /**
+     * 주어진 보드 이름에 따라 보드 정보를 조회하고 캐시합니다.
+     * 캐시된 데이터는 'boardInfo' 캐시에 저장되며, 보드 이름이 키로 사용됩니다.
+     *
+     * @param boardName 조회할 보드의 이름. null이면 시스템에 존재하는 모든 보드의 정보를 반환합니다.
+     * @return 주어진 이름의 보드 정보 리스트를 반환합니다.
+     *         각 정보에는 보드의 이름과 해당 보드에 속한 게시글 수가 포함됩니다.
+     */
     @Cacheable(value = "boardInfo", key = "#boardName")
     public List<BoardInfoResponse> findByBoardName(String boardName) {
         return boardInfoRepository.findBoardInfo(boardName);

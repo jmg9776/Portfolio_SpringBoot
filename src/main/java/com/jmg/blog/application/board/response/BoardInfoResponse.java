6a711f6cbe114b216c91f3aafbd3aa0902dd7e40
jmg97 <jmg97@kakao.com>
2023-12-29 21:49:00 +0900

    feat: 레디스 설정 업데이트
    - Java 직렬화를 Redis 직렬화로 변경
    - 반복된 'implements' 구문 삭제

--- src/main/java/com/jmg/blog/application/board/response/BoardInfoResponse.java
+++ src/main/java/com/jmg/blog/application/board/response/BoardInfoResponse.java
@@ -1,9 +1,7 @@
 package com.jmg.blog.application.board.response;
 
-import java.io.Serializable;
-
 public record BoardInfoResponse(
         String boardName,
         Long postCount
-) implements Serializable {
+) {
 }
---
+++ src/main/java/com/jmg/blog/infrastructure/config/RedisConfig.java
@@ -0,0 +1,63 @@
+package com.jmg.blog.infrastructure.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.CacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
+
+class RedisKeys {
+
+    public static final int DEFAULT_EXPIRE_SEC = 300;
+    public static final String NAME = "blog";
+
+}
+
+@Configuration
+public class RedisConfig {
+    @Value("${spring.data.redis.host}")
+    private String host;
+    @Value("${spring.data.redis.port}")
+    private int port;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(host, port);
+    }
+
+    private Map<String, RedisCacheConfiguration> cacheConfigurations() {
+        Map<String, RedisCacheConfiguration> cacheConfigurations = new HashMap<>();
+        cacheConfigurations.put(RedisKeys.NAME, RedisCacheConfiguration.defaultCacheConfig()
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()))
+                .entryTtl(Duration.ofSeconds(RedisKeys.DEFAULT_EXPIRE_SEC)));
+        return cacheConfigurations;
+    }
+
+    @Bean
+    public CacheManager cacheManager() {
+        RedisCacheManager.RedisCacheManagerBuilder builder = RedisCacheManager.RedisCacheManagerBuilder.fromConnectionFactory(redisConnectionFactory());
+        RedisCacheConfiguration configuration = RedisCacheConfiguration.defaultCacheConfig()
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()))
+                .entryTtl(Duration.ofSeconds(RedisKeys.DEFAULT_EXPIRE_SEC));
+        builder.cacheDefaults(configuration)
+                .withInitialCacheConfigurations(cacheConfigurations());
+        return builder.build();
+    }
+
+    @Bean
+    public RedisTemplate<?, ?> redisTemplate() {
+        RedisTemplate<byte[], byte[]> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        return redisTemplate;
+    }
+}

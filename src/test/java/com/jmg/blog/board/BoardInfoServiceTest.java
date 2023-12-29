86ddcb3dd449bd1da8454a96cfa48ec9c85e576e
jmg97 <jmg97@kakao.com>
2023-12-29 19:05:57 +0900

    feat: BoardInfoServiceTest에 이름과 카테고리별 보드 정보 검색 테스트 추가
    - 이름으로 보드 정보를 검색하는 boardInfoTest를 구현하여 'NO CONTENT', 일치하는 내용, 모든 내용 검색을 테스트합니다.
    - 카테고리로 보드 정보를 검색하는 boardInfoByCategoryTest를 구현하여 유효한 카테고리, 잘못된 카테고리, null 입력에 대해 검증합니다.

---
+++ src/test/java/com/jmg/blog/board/BoardInfoServiceTest.java
@@ -0,0 +1,67 @@
+package com.jmg.blog.board;
+
+import com.jmg.blog.application.board.response.BoardCategoryInfoResponse;
+import com.jmg.blog.application.board.response.BoardInfoResponse;
+import com.jmg.blog.application.board.service.BoardService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+public class BoardInfoServiceTest {
+
+    @Autowired
+    private BoardService boardService;
+    private static final Logger logger = LoggerFactory.getLogger(BoardInfoServiceTest.class);
+
+    @Test
+    @DisplayName("보드의 정보 가져오기")
+    void boardInfoTest() {
+        String searchText = "프로젝트";
+        List<BoardInfoResponse> noContentBoards = boardService.findByBoardName("NO CONTENT");
+        List<BoardInfoResponse> matchedBoards = boardService.findByBoardName(searchText);
+        List<BoardInfoResponse> allBoards = boardService.findByBoardName(null);
+
+        logger.info(noContentBoards.toString());
+        logger.info(matchedBoards.toString());
+        logger.info(allBoards.toString());
+
+        // 없는 컨텐츠 - noContentBoards 리스트가 비어있음을 확인
+        assertTrue(noContentBoards.isEmpty(), "리스트가 비어있지 않습니다. 'NO CONTENT'로 검색했을 때 결과가 없어야 합니다.");
+
+        // matchedBoards 리스트에 특정 요소가 포함되어 있는지 확인
+        assertTrue(matchedBoards.stream().anyMatch(board -> searchText.equals(board.boardName())),
+                "리스트에 '" + searchText + "'가 포함되어 있지 않습니다. '프로젝트'로 검색했을 때 해당 이름을 가진 보드가 최소 하나 이상 있어야 합니다.");
+    }
+
+    @Test
+    @DisplayName("카테고리 기준으로 보드 정보 가져오기")
+    void boardInfoByCategoryTest() {
+        // 테스트에 사용할 데이터 설정
+        String validCategory = "포트폴리오";
+        String invalidCategory = "존재하지않는카테고리";
+
+        // 올바른 카테고리로 검색
+        List<BoardCategoryInfoResponse> validResults = boardService.findByCategory(validCategory);
+        logger.info("올바른 카테고리로 검색한 결과: " + validResults.toString());
+        assertFalse(validResults.isEmpty(), "올바른 카테고리로 검색했을 때 결과가 비어있으면 안 됩니다.");
+
+        // 잘못된 카테고리로 검색
+        List<BoardCategoryInfoResponse> invalidResults = boardService.findByCategory(invalidCategory);
+        logger.info("잘못된 카테고리로 검색한 결과: " + invalidResults.toString());
+        assertTrue(invalidResults.isEmpty(), "잘못된 카테고리로 검색했을 때 결과가 비어있어야 합니다.");
+
+        // null로 검색
+        List<BoardCategoryInfoResponse> nullResults = boardService.findByCategory(null);
+        logger.info("카테고리 없이 검색한 결과: " + nullResults.toString());
+        assertNotNull(nullResults, "카테고리 없이 검색한 결과는 null이 아니어야 합니다.");
+    }
+
+}

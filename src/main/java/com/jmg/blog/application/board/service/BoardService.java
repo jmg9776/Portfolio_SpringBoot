55601831a008054092a2add28e5f3e94cb866c75
jmg97 <jmg97@kakao.com>
2023-12-29 19:07:44 +0900

    feat: 캐싱을 통한 BoardService 조회 추가 및 성능 최적화
    - 카테고리별 보드 정보 조회를 위한 findByCategory 메서드에 @Cacheable 적용. 'boardCategoryInfo' 캐시에 결과를 저장하여 동일 카테고리에 대한 반복 요청 시 성능 향상.
    - 보드 이름별 정보 조회를 위한 findByBoardName 메서드에도 @Cacheable 적용. 'boardInfo' 캐시에 결과를 저장하여 동일 보드 이름에 대한 반복 요청 시 데이터 접근 시간 감소.

--- src/main/java/com/jmg/blog/application/board/service/BoardService.java
+++ src/main/java/com/jmg/blog/application/board/service/BoardService.java
@@ -1,8 +1,10 @@
 package com.jmg.blog.application.board.service;
 
 import com.jmg.blog.application.board.response.BoardCategoryInfoResponse;
+import com.jmg.blog.application.board.response.BoardInfoResponse;
 import com.jmg.blog.domain.board.repository.BoardInfoRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,7 +14,13 @@ import java.util.List;
 public class BoardService {
     private final BoardInfoRepository boardInfoRepository;
 
-    public List<BoardCategoryInfoResponse> getBoardList(String category) {
+    @Cacheable(value = "boardCategoryInfo", key = "#category")
+    public List<BoardCategoryInfoResponse> findByCategory(String category) {
         return boardInfoRepository.findBoardInfoListByBoardCategory(category);
+    }
+
+    @Cacheable(value = "boardInfo")
+    public List<BoardInfoResponse> findByBoardName(String boardName) {
+        return boardInfoRepository.findBoardInfo(boardName);
     }
 }

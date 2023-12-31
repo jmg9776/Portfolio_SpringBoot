15f9110983ec19c59bc3227b8f631a29b19c6923
mingyun <jmg97@kakao.com>
2023-12-31 17:21:53 +0900

    feat: getBoardInfoAllByCategory, BoardCategoryInfoAllResponse 전체 검색 컨트롤러 분리 및 BoardCategoryInfoAllResponse DTO 추가 API 사용성 개선

--- src/main/java/com/jmg/blog/presentation/controller/board/BoardInfoController.java
+++ src/main/java/com/jmg/blog/presentation/controller/board/BoardInfoController.java
@@ -1,6 +1,7 @@
 package com.jmg.blog.presentation.controller.board;
 
-import com.jmg.blog.application.board.response.BoardCategoryInfoResponse;
+import com.jmg.blog.presentation.controller.board.response.BoardCategoryInfoAllResponse;
+import com.jmg.blog.presentation.controller.board.response.BoardCategoryInfoResponse;
 import com.jmg.blog.application.board.service.BoardInfoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -15,8 +16,19 @@ import java.util.List;
 public class BoardInfoController {
     private final BoardInfoService boardInfoService;
 
-    @GetMapping(value = {"/category/{categoryName}", "/category"})
+    @GetMapping(value = {"/category/{categoryName}"})
     public ResponseEntity<List<BoardCategoryInfoResponse>> getBoardInfoByCategory(@PathVariable(required = false) String categoryName) {
         return ResponseEntity.ok().body(boardInfoService.findByCategory(categoryName));
+    }
+
+    @GetMapping(value = {"/category"})
+    public ResponseEntity<BoardCategoryInfoAllResponse> getBoardInfoAllByCategory() {
+        List<BoardCategoryInfoResponse> boardCategoryInfoResponseList = boardInfoService.findByCategory(null);
+
+        long sum = boardCategoryInfoResponseList.stream()
+                .mapToLong(BoardCategoryInfoResponse::count)
+                .sum();
+
+        return ResponseEntity.ok().body(new BoardCategoryInfoAllResponse(sum, boardCategoryInfoResponseList));
     }
 }

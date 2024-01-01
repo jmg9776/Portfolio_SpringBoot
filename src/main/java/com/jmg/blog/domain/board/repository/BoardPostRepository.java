422198251cd9cbef3c10620fac66b1910562c621
mingyun <jmg97@kakao.com>
2024-01-01 20:49:07 +0900

    refactor: BoardPostRepository에서 BoardPostRepositoryCustom 확장 제거
    BoardPostRepository에서 더 이상 BoardPostRepositoryCustom을 확장하지 않도록 수정함

--- src/main/java/com/jmg/blog/domain/board/repository/BoardPostRepository.java
+++ src/main/java/com/jmg/blog/domain/board/repository/BoardPostRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BoardPostRepository extends JpaRepository<BoardPost, Long>, BoardPostRepositoryCustom {
+public interface BoardPostRepository extends JpaRepository<BoardPost, Long> {
 }

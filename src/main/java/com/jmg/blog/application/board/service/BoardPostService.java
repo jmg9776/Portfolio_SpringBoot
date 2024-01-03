49124fd2ce4904feda9d8d874d5c1c695f36db34
mingyun <jmg97@kakao.com>
2024-01-03 21:10:13 +0900

    feat : transactional 추가

--- src/main/java/com/jmg/blog/application/board/service/BoardPostService.java
+++ src/main/java/com/jmg/blog/application/board/service/BoardPostService.java
@@ -2,6 +2,7 @@ package com.jmg.blog.application.board.service;
 
 import com.jmg.blog.domain.board.repository.BoardPostRepositoryCustom;
 import com.jmg.blog.presentation.controller.board.response.BoardPostResponse;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
@@ -12,6 +13,7 @@ import org.springframework.stereotype.Service;
 public class BoardPostService {
     private final BoardPostRepositoryCustom boardPostRepositoryCustom;
 
+    @Transactional
     public PageImpl<BoardPostResponse> findBoardPostListByBoardName(String boardName, Pageable pageable) {
         return boardPostRepositoryCustom.findBoardPostListByBoardName(boardName, pageable);
     }
